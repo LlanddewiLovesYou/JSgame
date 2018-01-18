7f078af24e507e50d5779a16d6c81f24482e6492
Ian Del Duca <ianmdelduca@gmail.com>
2018-01-18 17:57:47 -0500

    end of day wed, futzed with front end

--- bundle.js
+++ bundle.js
@@ -101,14 +101,6 @@ var Board = function () {
     _classCallCheck(this, Board);
 
     this.grid = this.fill();
-    // this.col1 = this.grid[0]
-    // this.col2 = this.grid[1]
-    // this.col3 = this.grid[2]
-    // this.col4 = this.grid[3]
-    // this.col5 = this.grid[4]
-    // this.col6 = this.grid[5]
-    // this.col7 = this.grid[6]
-    // this.col8 = this.grid[7]
     this.fill = this.fill.bind(this);
     this.check3Vert = this.check3Vert.bind(this);
   }
