34350686394c0bb06f447e53dce4429b8043a8b0
Ian Del Duca <ianmdelduca@gmail.com>
2018-01-21 12:54:26 -0500

    reverted scss to css for github page

--- bundle.js
+++ bundle.js
@@ -92,6 +92,10 @@ var _createClass = function () { function defineProperties(target, props) { for 
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
+// import  DOMNodeCollection from "./YayQuery/dom_node_collection";
+// import YayQuery from "./YayQuery/main";
+
+
 var emptyGrid = [[null, null, null, null, null, null, null, null], [null, null, null, null, null, null, null, null], [null, null, null, null, null, null, null, null], [null, null, null, null, null, null, null, null], [null, null, null, null, null, null, null, null], [null, null, null, null, null, null, null, null], [null, null, null, null, null, null, null, null], [null, null, null, null, null, null, null, null]];
 
 var classes = ["F", "W", "C", "R", "X"];
@@ -103,32 +107,101 @@ var Board = function () {
     this.grid = this.fill();
     this.fill = this.fill.bind(this);
     this.check3Vert = this.check3Vert.bind(this);
+    this.move = [];
   }
+
+  // NOTE: BOARD SETUP
+
 
   _createClass(Board, [{
     key: "fill",
     value: function fill() {
       var newGrid = emptyGrid.slice(0);
       this.grid = newGrid;
-      return newGrid.map(function (column) {
+      this.grid = newGrid.map(function (column) {
         return column.map(function (space) {
           if (space === null) {
             return space = classes[Math.floor(Math.random() * classes.length)];
           }
         });
       });
+      this.addHeroes();
+      $l('.gameboard').on('click', this.makeMove.bind(this));
+      return this.grid;
     }
   }, {
+    key: "makeMove",
+    value: function makeMove(click) {
+      var clickPos = click.target;
+      var pos = void 0;
+      if (!clickPos.id) {
+        pos = clickPos.parentElement.id.split(',');
+        console.log(pos);
+      } else {
+        pos = clickPos.id.split(',');
+        console.log(pos);
+      }
+      this.move.push(pos);
+      if (this.move.length === 2) {
+        this.swapHeroes(this.move[0], this.move[1]);
+        this.move = [];
+      }
+    }
+  }, {
+    key: "addHeroes",
+    value: function addHeroes() {
+      var DOMel = void 0;
+      for (var i = 0; i < 8; i++) {
+        for (var j = 0; j < this.grid[i].length; j++) {
+          var heroClass = this.grid[i][j];
+          var pos = [i, j];
+          this.addHero(heroClass, pos);
+        }
+      }
+    }
+  }, {
+    key: "addHero",
+    value: function addHero(heroClass, pos) {
+      var i = pos[0];
+      var j = pos[1];
+      var DOMel = void 0;
+      switch (heroClass) {
+        case 'F':
+          DOMel = $l("#" + i + "," + j);
+          DOMel.append('<img class="hero" src="./assets/fighter.png"></img>');
+          break;
+        case 'W':
+          DOMel = $l("#" + i + "," + j);
+          DOMel.append('<img class="hero" src="./assets/blackmage.png"></img>');
+          break;
+        case 'C':
+          DOMel = $l("#" + i + "," + j);
+          DOMel.append('<img class="hero" src="./assets/cleric.png"></img>');
+          break;
+        case 'R':
+          DOMel = $l("#" + i + "," + j);
+          DOMel.append('<img class="hero" src="./assets/rogue.png"></img>');
+          break;
+        case 'X':
+          DOMel = $l("#" + i + "," + j);
+          DOMel.append('<img class="hero" src="./assets/chocobo.png"></img>');
+          break;
+      }
+    }
+
+    // NOTE: MATCH CHECKING
+
+  }, {
     key: "check3Vert",
-    value: function check3Vert() {
+    value: function check3Vert(grid) {
       //checks the board for combinations of three in each COLUMN
       var newGrid = this.grid.slice(0);
       return newGrid.map(function (column, i) {
-        return column.map(function (element, i) {
-          if (element === column[i + 1] && element === column[i + 2]) {
-            column[i] = 'match';
-            column[i + 1] = 'match';
-            column[i + 2] = 'match';
+        return column.map(function (element, j) {
+          if (element === column[j + 1] && element === column[j + 2]) {
+            column[j] = 'match';
+            column[j + 1] = 'match';
+            column[j + 2] = 'match';
             return 'match';
           } else {
             return element;
@@ -141,55 +214,94 @@ var Board = function () {
     key: "check3Horz",
     value: function check3Horz() {
       //checks the board for combinations of three in each ROW
-      var transposed = this;
-      transposed.grid = transposed.grid.transpose();
-      var checked = transposed.check3Vert();
-      return checked.transpose();
+      this.grid = this.grid.transpose();
+      this.check3Vert();
+      this.grid = this.grid.transpose();
+      return this.grid;
     }
   }, {
     key: "remove3",
     value: function remove3() {
       //removes 3 matching pieces from the board
-      var newGrid = this.grid.map(function (column) {
-        column = column.removeMatches();
+      var newGrid = this.grid.map(function (column, i) {
+        column = column.removeMatches(i);
         return column;
       });
       this.grid = newGrid;
     }
   }, {
+    key: "columnGravity",
+    value: function columnGravity() {
+      for (var i = 0; i < 8; i++) {
+        if (this.grid[i].length < 8) {
+          for (var j = 0; j < 8; j++) {
+            $l("#" + i + "," + j).empty();
+            if (this.grid[i][j]) {
+              this.addHero(this.grid[i][j], [i, j]);
+            }
+          }
+        }
+      }
+    }
+  }, {
     key: "refill",
     value: function refill() {
+      var _this = this;
+
       var newGrid = this.grid.slice(0);
-      this.grid.map(function (column) {
+      this.columnGravity();
+      this.grid.map(function (column, i) {
+        debugger;
         while (column.length < 8) {
-          column.push(classes[Math.floor(Math.random() * classes.length)]);
+          debugger;
+          var heroClass = classes[Math.floor(Math.random() * classes.length)];
+          column.push(heroClass);
+          _this.addHero(heroClass, [i, column.length - 1]);
         }
       });
+    }
+
+    // NOTE: MOVE HANDLING
+
+  }, {
+    key: "swapHeroes",
+    value: function swapHeroes(select1, select2) {
+      var space1 = this.grid[select1[0]][select1[1]].slice(0);
+      var space2 = this.grid[select2[0]][select2[1]].slice(0);
+      this.grid[select1[0]][select1[1]] = space2;
+      this.grid[select2[0]][select2[1]] = space1;
+      $l("#" + select2[0] + "," + select2[1]).empty();
+      $l("#" + select1[0] + "," + select1[1]).empty();
+      this.addHero(space1, select2);
+      this.addHero(space2, select1);
     }
   }]);
 
   return Board;
-}(); //class Grid
+}(); //class Board
 
 
 //HELPER METHODS
 
 
-Array.prototype.removeMatches = function () {
+Array.prototype.removeMatches = function (i) {
   var newArray = [];
-  this.forEach(function (el) {
+  this.forEach(function (el, j) {
     if (el !== 'match') {
       newArray.push(el);
+    } else {
+      debugger;
+      $l("#" + i + "," + j).empty();
     }
   });
   return newArray;
 };
 
 Array.prototype.transpose = function () {
-  var _this = this;
+  var _this2 = this;
 
   var columns = Array.from({ length: this[0].length }, function () {
-    return Array.from({ length: _this.length });
+    return Array.from({ length: _this2.length });
   });
   for (var i = 0; i < this.length; i++) {
     for (var j = 0; j < this[i].length; j++) {
@@ -198,7 +310,8 @@ Array.prototype.transpose = function () {
   }
   return columns;
 };
-// export default Board;
+
+// export.module Board;
 
 /***/ })
 /******/ ]);
